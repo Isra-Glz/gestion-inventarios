9a40e0e2f373b66cd72511666674a3159a6fd6ea
Isra-Glz <isra.gonzalez.curiel@gmail.com>
2025-12-09 23:10:08 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,19 @@
+from fastapi import FastAPI
+from app.routes import inventory_routes  # lo crearemos después
+from app.routes import users_routes      # lo crearemos después
+
+app = FastAPI(
+    title="API de Gestión de Inventario",
+    description="Backend con Python, FastAPI y SQL Server",
+    version="1.0.0"
+)
+
+# Registrar rutas
+app.include_router(inventory_routes.router, prefix="/inventario", tags=["Inventario"])
+app.include_router(users_routes.router, prefix="/usuarios", tags=["Usuarios"])
+
+# Ruta de prueba
+@app.get("/")
+def root():
+    return {"message": "API funcionando correctamente"}
 
